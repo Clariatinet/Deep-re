95ced1e293047bb8e872f0ae1bd731443d839e6f
sungmin <95615086+Clariatinet@users.noreply.github.com>
2023-07-18 23:53:49 +0900

    7/18 딥다이브 복습

--- 3.dataType/number1.js
+++ 3.dataType/number1.js
@@ -11,4 +11,4 @@ console.log(3 / 2); // 1.5
 // 숫자 타입의 세 가지 특별한 값
 console.log(10 / 0); // Infinity
 console.log(10 / -0); // -Infinity
-console.log(1 * 'String'); // NaN
+console.log(1 * 'String'); // NaN (Not a Number)
